4eafea56b71861b82864d1a3baf1f5bff5aff9f2
Necris45 <necris01@mail.ru>
2022-09-29 05:02:17 +0500

    2 дз выполнено

--- dz2/Lenta parsing.py
+++ dz2/Lenta parsing.py
@@ -11,16 +11,19 @@ headers = {
 
 response = requests.get(url=url, headers=headers)
 dom = html.fromstring(response.text)
+# Вытаскиваем новости
 news = dom.xpath(".//*[@class='card-mini _topnews']")
 news_list = []
 for n in news:
     news_dict = {}
+    # заголовок
     n_title = n.xpath(".//span[@class='card-mini__title']/text()")
     if len(n_title) == 1:
         n_title = n_title[0]
     else:
         n_title = None
     news_dict['title'] = n_title
+    # ссылка и источник (во время экспериментов попадался другой источник, а не lenta.ru)
     href = n.xpath(".//@href")
     source = None
     if len(href) == 1:
@@ -35,6 +38,7 @@ for n in news:
         href = None
     news_dict['href'] = href
     news_dict['source'] = source
+    # дата публикации. Если есть только время, значит дата текущая
     time = n.xpath(".//*[@class='card-mini__date']/text()")
     if len(time[0]) > 5:
         public_time = time[0]
@@ -45,8 +49,6 @@ for n in news:
     news_dict['public_time'] = public_time
     news_list.append(news_dict)
 
-print(news_list)
 data = json.dumps(news_list, ensure_ascii=False)
-print(data)
 with open('news_parsing_data.json', 'w') as outfile:
     outfile.write(data)
